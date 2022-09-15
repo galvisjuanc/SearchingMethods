2ae3b7778b5e42e86806611e7be3fada1b93dca6
Juan <galvisjuanc@gmail.com>
2022-09-15 09:12:05 -0500

    LinearSearch Simple and with Interface

---
+++ src/com/mycompany/searching/BusquedaSecuencial.java
@@ -0,0 +1,36 @@
+package com.mycompany.searching;
+
+import jdk.nashorn.internal.scripts.JO;
+
+import javax.swing.*;
+
+/**
+ * @author galvi
+ * Busqueda secuencial:
+ * Suponiendo un arreglo [] = {3, 2, 5, 1, 4}; dato = 2;
+ */
+public class BusquedaSecuencial {
+    public static void main(String[] args) {
+        int arreglo[] = {4, 2, 3, 5, 1};
+        int dato;
+        boolean bandera = false;
+
+        dato = Integer.parseInt(JOptionPane.showInputDialog("Digite el número a buscar en el arreglo: "));
+
+        //Busqueda Secuencial
+        int i = 0;
+        while(i < 5 && bandera == false){
+            if(arreglo[i] == dato) {
+                bandera = true;
+            }
+            i++;
+        }
+        if(bandera == false){
+            JOptionPane.showMessageDialog(null, "El número no se encuentra en el arreglo");
+        }
+        else{
+            JOptionPane.showMessageDialog(null, "El numero se encontró en la posición: "+(i-1)+" del arreglo");
+        }
+
+    }
+}
